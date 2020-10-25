0acb9f2be862bf45914fa742a2e44b40458eed23
Lennox <omondilenox@outlook.com>
2020-10-25 17:09:25 +0300

    added static files to base layout

--- awwardsClone/settings.py
+++ awwardsClone/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'rest_framework',
     'cloudinary',
     'api.apps.ApiConfig',
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS =[
+    BASE_DIR / 'awwardsClone/static'
+]
