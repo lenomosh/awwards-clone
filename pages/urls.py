15b6da9774261bb6711bc06afaba12c03254ab37
Lennox <omondilenox@outlook.com>
2020-10-30 09:23:01 +0300

    created url patterns for the views

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from pages import views
-urlpatterns =[
-    path('index',views.index, name='pages.index')
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='pages-home'),
+    path('project/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('project/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('project/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('project/new/', PostCreateView.as_view(), name='post-create'),
+
 ]
