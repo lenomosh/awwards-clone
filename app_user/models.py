fe6baa6ffd743a11e4c6042885cafe220cd78574
Lennox <omondilenox@outlook.com>
2020-10-30 09:26:25 +0300

    created classes for project models

--- app_user/models.py
+++ app_user/models.py
@@ -1,48 +1,21 @@
-from django.contrib.auth.models import User
 from django.db import models
-from cloudinary.models import CloudinaryField
-import math
-
-
-# Create your models here.
-class Project(models.Model):
-    url = models.URLField()
-    description = models.TextField()
-    title = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = CloudinaryField("image")
-    CREATED_AT = models.DateTimeField(auto_now_add=True)
-    UPDATED_AT = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['UPDATED_AT']
-
-    def __str__(self):
-        return self.title
+from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
-    bio = models.CharField(max_length=255)
-    profile_picture = CloudinaryField("image")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    CREATED_AT = models.DateTimeField(auto_now_add=True)
-    UPDATED_AT = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['UPDATED_AT']
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
 
     def __str__(self):
-        return self.user.first_name
+        return f'{self.user.username} Profile'
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-class Rating(models.Model):
-    usability, design, content = models.IntegerField(), models.IntegerField(), models.IntegerField()
-    CREATED_AT = models.DateTimeField(auto_now_add=True)
-    UPDATED_AT = models.DateTimeField(auto_now=True)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+        img = Image.open(self.image.path)
 
-    class Meta:
-        ordering = ['UPDATED_AT']
-
-    def __str__(self):
-        return math.fsum([self.usability, self.design, self.content])/3
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+# from PIL import Image
 
-# Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    url = models.CharField(max_length=50, blank=True)
+
+   
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+
+# class Review(models.Model):
+#     ratings = (1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9),(10, 10)
+    
+#     user = models.ForeignKey(User,on_delete=models.CASCADE)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reviews')
+#     design = models.IntegerField(choices=ratings, default=0)
+#     usability = models.IntegerField(choices=ratings, default=0)
+#     creativity = models.IntegerField(choices=ratings, default=0)
+#     content =  models.IntegerField(choices=ratings, default=0)
+#     overall_score = models.IntegerField(blank=True, default=0)
+
+#     def save_rate(self):
+#             self.save()
+
+#     def delete_rate(self):
+#         self.delete()
+
+#     def get_absolute_url(self):
+#         return reverse('index')
